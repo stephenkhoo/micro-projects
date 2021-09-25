fc76a768d73c0bf3192f6cf989337a399c34021d
Khoo Yong Jun <programmer.sk95@gmail.com>
2021-09-25 17:09:56 +0800

    restructure to make this applicable for different projects

--- vue.config.js
+++ vue.config.js
@@ -3,63 +3,20 @@
 /**
  * @type {import('@vue/cli-service').ProjectOptions}
  */
+
+function page(name, entry, filename, title) {
+  return {
+    template: 'public/index.html',
+    entry, filename, title,
+    chunks: ['chunk-vendors', 'chunk-common', name]
+  }
+}
+
 module.exports = {
   pages: {
-    index: {
-      // entry for the page
-      entry: 'src/main.js',
-      // the source template
-      template: 'public/index.html',
-      // output as dist/index.html
-      filename: 'index.html',
-      // when using title option,
-      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'Landing',
-      // chunks to include on this page, by default includes
-      // extracted common chunks and vendor chunks.
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
-    },
-    entry: {
-      // entry for the page
-      entry: 'src/entry.js',
-      // the source template
-      template: 'public/index.html',
-      // output as dist/index.html
-      filename: 'entry.html',
-      // when using title option,
-      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'Entry',
-      // chunks to include on this page, by default includes
-      // extracted common chunks and vendor chunks.
-      chunks: ['chunk-vendors', 'chunk-common', 'entry']
-    },
-    report: {
-      // entry for the page
-      entry: 'src/report.js',
-      // the source template
-      template: 'public/index.html',
-      // output as dist/index.html
-      filename: 'report.html',
-      // when using title option,
-      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'Entry',
-      // chunks to include on this page, by default includes
-      // extracted common chunks and vendor chunks.
-      chunks: ['chunk-vendors', 'chunk-common', 'report']
-    },
-    record: {
-      // entry for the page
-      entry: 'src/record.js',
-      // the source template
-      template: 'public/index.html',
-      // output as dist/index.html
-      filename: 'record.html',
-      // when using title option,
-      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'Entry',
-      // chunks to include on this page, by default includes
-      // extracted common chunks and vendor chunks.
-      chunks: ['chunk-vendors', 'chunk-common', 'record']
-    },
+    index: page('index', 'src/v-system/main.js', 'v-system/index.html', 'V-system Landing'),
+    entry: page('entry', 'src/v-system/entry.js', 'v-system/entry.html', 'V-system Entry'),
+    report: page('report', 'src/v-system/report.js', 'v-system/report.html', 'V-system Report'),
+    record: page('record', 'src/v-system/record.js', 'v-system/record.html', 'V-system Record'),
   }
 }
