e87e6e0f567dcc4a5502e2d91f4b204b1d534a80
Khoo Yong Jun <programmer.sk95@gmail.com>
2021-10-07 17:20:23 +0800

    better ux for iphone se

--- src/cc/components/Dashboard1/Order.vue
+++ src/cc/components/Dashboard1/Order.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="group h-12 p-2 flex items-center space-x-2 hover:bg-blue-500 hover:text-white">
-    <div class="flex-1 group-hover:text-white">{{ name }}</div>
-    <div class="w-2/12 group-hover:text-white">${{ price }}</div>
-    <div class="w-2/12 group-hover:text-white">{{ payment }}</div>
-    <div class="w-3/12 text-white flex justify-end">
+  <div class="group sm:h-12 p-2 flex flex-col sm:flex-row sm:items-center sm:space-x-2 hover:bg-blue-500 hover:text-white border-b border-b-gray-300">
+    <div class="flex sm:block sm:flex-1 group-hover:text-white">
+      <div class="sm:hidden font-bold w-24 inline-block after:content-[':']">Name</div>
+      <div class="flex-1">{{ name }}</div>
+    </div>
+    <div class="flex sm:block sm:w-2/12 group-hover:text-white">
+      <div class="sm:hidden font-bold w-24 inline-block after:content-[':']">Price</div>
+      <div class="flex-1">${{ price }}</div>
+    </div>
+    <div class="flex sm:block sm:w-2/12 group-hover:text-white">
+      <div class="sm:hidden font-bold w-24 inline-block after:content-[':']">Payment</div>
+      <div class="flex-1">{{ payment }}</div>
+    </div>
+    <div class="sm:w-3/12 text-white flex sm:justify-end">
+      <div class="sm:hidden text-black font-bold w-24 inline-block after:content-[':']">Status</div>
       <span :class="`${status.bgColor} px-1 rounded-md capitalize`">{{ status.text }}</span>
     </div>
   </div>
--- src/cc/components/Dashboard1/RecentOrders.vue
+++ src/cc/components/Dashboard1/RecentOrders.vue
@@ -5,7 +5,7 @@
       <button class="capitalize text-white py-1 px-2 bg-blue-500 rounded-md">view all</button>
     </div>
     <div class="flex-1 w-full overflow-y-auto">
-      <div class="font-bold capitalize h-12 p-2 flex items-center space-x-2 hover:bg-blue-500 hover:text-white">
+      <div class="font-bold capitalize h-12 p-2 hidden sm:flex items-center space-x-2 hover:bg-blue-500 hover:text-white">
         <div class="flex-1">name</div>
         <div class="w-2/12">price</div>
         <div class="w-2/12">payment</div>
