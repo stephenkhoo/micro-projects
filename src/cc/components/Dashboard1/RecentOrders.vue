d1725faeeb9f6737b97aff33f91346d7e5a997a8
Khoo Yong Jun <programmer.sk95@gmail.com>
2021-10-07 18:38:24 +0800

    flex-shrink-0 for safari

--- src/cc/components/Dashboard1/RecentOrders.vue
+++ src/cc/components/Dashboard1/RecentOrders.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="recent-orders" class="w-full lg:flex-1 flex flex-col shadow-md px-4 pt-2 py-1 md:p-6 md:pb-5 rounded-md">
-    <div class="w-full my-1 flex justify-between">
+    <div class="flex-shrink-0 w-full my-1 flex justify-between">
       <div class="text-blue-500 font-bold text-xl capitalize">recent orders</div>
       <button class="capitalize text-white py-1 px-2 bg-blue-500 rounded-md">view all</button>
     </div>
